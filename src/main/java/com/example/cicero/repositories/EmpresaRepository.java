73f3bccc1ce5adc91ae6ac9f437f13c89c46e48b
ciceroQa <ciceroqa0@gmail.com>
2022-01-13 22:35:40 -0300

    add repositories e conexao com banco completa h2 e mysql

---
+++ src/main/java/com/example/cicero/repositories/EmpresaRepository.java
@@ -0,0 +1,11 @@
+package com.example.cicero.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+import com.example.cicero.entities.Empresa;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long>{
+
+	@Transactional(readOnly=true)
+	Empresa findByCnpj(String cnpj);
+}
